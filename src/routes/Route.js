0310dcfba00c17374f3826798f23ec13b9c7cae7
@imrookhan <khannihar921@gmail.com>
2022-11-29 11:21:38 +0500

    fix the api

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 const BoardController = require('../controllers/BoardControllers.js');
 router.post('/user/data', BoardController.createBoard);
-router.get('/user/data', BoardController.getBoards);
+router.get('/user/alldata', BoardController.getBoards);
 router.put('/user/data/:_id', BoardController.updateRoadmap);
 router.delete('/user/data/:_id', BoardController.deleteRoadmap);
 module.exports = router;
