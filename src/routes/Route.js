18d5bd4cf0c6a9c7da7c905ca06dc360fdc4bb69
@imrookhan <khannihar921@gmail.com>
2022-11-30 12:49:54 +0500

    fix the code

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router();
 const BoardController = require('../controllers/BoardControllers.js');
 const AuthControllers = require('../controllers/AuthControllers.js');
+router.get('/', ((req, res) => {
+  res.send("welcome to roadmap");
+}));
+
 router.post('/register', AuthControllers.signupUser);
 router.post('/login', AuthControllers.loginUser);
 router.post('/user/data', BoardController.createBoard);
