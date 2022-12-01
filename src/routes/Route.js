d4e21a4e6fee98d449e041415c1c3e7543d58701
zeeshanromi00 <zkromeo6666@gmail.com>
2022-12-01 09:40:59 +0500

    SignUp Email verification Done

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,9 +1,10 @@
-const express = require('express')
+  const express = require('express')
 const router = express.Router();
 const BoardController = require('../controllers/BoardControllers.js');
 const AuthControllers = require('../controllers/AuthControllers.js');
 router.post('/register', AuthControllers.signupUser);
 router.post('/login', AuthControllers.loginUser);
+router.get('/verify', AuthControllers.verifyMail)
 router.post('/user/data', BoardController.createBoard);
 router.get('/user/data', BoardController.getBoards);
 router.put('/user/data/:_id', BoardController.updateRoadmap);
